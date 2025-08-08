54c8f78a848691874e4b8b8d7e7e6e5e1fc305d9
LemonBunz <72637502+LemonBunz@users.noreply.github.com>
2025-08-08 16:26:36 +0800

    Update TBGame.lua

--- ReplicatedStorage/Classes/TBGame.lua
+++ ReplicatedStorage/Classes/TBGame.lua
@@ -1,4 +1,4 @@
-zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzlocal TurnsManager = require(script.Parent.TurnsManager);
+local TurnsManager = require(script.Parent.TurnsManager);
 local StateMachine = require(script.Parent.StateMachine)
 local RunService = game:GetService("RunService")
 
@@ -15,17 +15,40 @@ function TBGame:new(name, initialStateName, currentTeam, initialturnsTable)
 end
 
 function TBGame:PositionTeam(teamsFolder, teamPositionsFolder)
-	for i, teamMember in pairs(teamsFolder:GetChildren()) do
-		local teams = teamsFolder:GetChildren();
-		local teamPositions = teamsFolder:GetChildren();
+	local currentTeam = self.TurnsManager.CurrentTeam;
+	local teamPositions = teamPositionsFolder:GetChildren()
+	
+	for teamName, team in pairs(currentTeam) do
+		for _, teamPosition in ipairs(teamPositions) do
+			local totalSlots = #teamPosition:GetChildren()
+			if teamName == "Team1" and teamPosition.Name == "Team1Positions" then
+				local position = math.ceil(totalSlots/#team)
+				for i, character in ipairs(team) do
+					local slot = teamPosition:FindFirstChild("Slot"..position)
+					character.Model.HumanoidRootPart.CFrame = CFrame.new(slot.CFrame.Position)
+					position += 1
+				end
+				
+			elseif teamName == "Team2" and teamPosition.Name == "Team2Positions" then
+				local position = math.ceil(totalSlots/#team)
+				for i, character in ipairs(team) do
+					local slot = teamPosition:FindFirstChild("Slot"..position)
+					character.Model.HumanoidRootPart.CFrame = CFrame.new(slot.CFrame.Position) * CFrame.Angles(0, math.rad(180), 0)
+					position += 1
+				end
+			end
+		end
 	end
-end
+end	
 
 function TBGame:Start() 
 	RunService.Heartbeat:Connect(function(T) 
+		print(self.StateMachine)
 		self.StateMachine:Update()
 	end)
 end
 
+
 return TBGame
+
 
