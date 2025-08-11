4dff92d5d9e3ce5cad8a159a215f7c71f5052827
LemonBunz <72637502+LemonBunz@users.noreply.github.com>
2025-08-11 14:19:58 +0800

    Update PlayerTurn.lua

--- GameState/PlayerTurn.lua
+++ GameState/PlayerTurn.lua
@@ -1,13 +1,19 @@
 local PlayerTurn = {}
-function PlayerTurn:OnEnter() 
+function PlayerTurn:OnEnter(tbgame) 
 	print("Entered PlayerTurn State")
 end
 
-function PlayerTurn:OnUpdate() 
-	print("Updating PlayerTurn State")
+function PlayerTurn:OnUpdate(tbgame) 
+	local stateMachine = tbgame.StateMachine
+	local contestants = tbgame.Contestants
+	local player1 = contestants.Player1
+	
+	stateMachine:ChangeState("EnemyTurn") -- Change State
+	stateMachine:Update() --Update NewState:OnUpdate()
 end
 
-function PlayerTurn:OnExit() 
+function PlayerTurn:OnExit(tbgame) 
 	print("Exited PlayerTurn State")
 end
 return PlayerTurn
+
