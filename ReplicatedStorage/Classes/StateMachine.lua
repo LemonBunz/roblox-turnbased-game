fc285df8b8960c5989f831125dfae88d27c48faa
LemonBunz <72637502+LemonBunz@users.noreply.github.com>
2025-08-11 14:15:17 +0800

    Update StateMachine.lua

--- ReplicatedStorage/Classes/StateMachine.lua
+++ ReplicatedStorage/Classes/StateMachine.lua
@@ -1,42 +1,52 @@
-local GameState = {
-	Idle = require(script.Idle),
-	PlayerTurn = require(script.PlayerTurn),
-	EnemyTurn = require(script.EnemyTurn),
-	GameOver = require(script.GameOver)
-}
+local remoteEvents = game.ReplicatedStorage.RemoteEvents
 
+local function safeCall(callback, ...)
+	if typeof(callback) ~= "function" then
+		return
+	end
+
+	local success, err = pcall(callback, ...)
+	if not success then
+		warn("State callback error:", err)
+	end
+end
 local StateMachine = {}
 StateMachine.__index = StateMachine
 
 -- Constructor
-function StateMachine:new(initialStateName)
+function StateMachine:new(initialStateName, TbGame)
 	local self = setmetatable({}, StateMachine)
-	self.CurrentState = GameState[initialStateName]
+	self.GameState = {
+		Idle = require(script.Idle),
+		PlayerTurn = require(script.PlayerTurn),
+		EnemyTurn = require(script.EnemyTurn),
+		GameOver = require(script.GameOver)
+	}
+
+	self.CurrentState = self.GameState[initialStateName]
 	self.CurrentStateName = initialStateName
+	self.TbGame = TbGame
     return self
 end
 -- Methods
 function StateMachine:ChangeState(newStateName)
-	if self.CurrentState and self.CurrentState.OnExit then 
-		self.CurrentState.OnExit()
+	if self.CurrentState then
+		safeCall(self.CurrentState.OnExit, self.CurrentState, self.TbGame)
 	end
-	
-	local newState = GameState[newStateName]
-	print(newStateName)
+
+	local newState = self.GameState[newStateName]
 	assert(newState, "State ".. tostring(newStateName).." not found")
 
+
+	
 	self.CurrentState = newState
 	self.CurrentStateName = newStateName
-	
-	if self.CurrentState and self.CurrentState.OnEnter then 
-		self.CurrentState.OnEnter()
-	end
+	safeCall(self.CurrentState.OnEnter, self.CurrentState, self.TbGame)
 end
 
-
 function StateMachine:Update()
-	if self.CurrentState and self.CurrentState.OnUpdate then 
-		self.CurrentState.OnUpdate()
+	if self.CurrentState then
+		safeCall(self.CurrentState.OnUpdate, self.CurrentState, self.TbGame)
 	end
 end
 
