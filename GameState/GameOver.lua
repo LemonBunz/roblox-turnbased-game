b4d7f55452f12bcc1e30c46215d8a6ba738323cd
LemonBunz <72637502+LemonBunz@users.noreply.github.com>
2025-08-11 14:21:00 +0800

    Update GameOver.lua

--- GameState/GameOver.lua
+++ GameState/GameOver.lua
@@ -1,13 +1,20 @@
 local GameOver = {}
-function GameOver:OnEnter() 
+function GameOver:OnEnter(tbgame) 
 	print("Entered GameOver State")
 end
 
-function GameOver:OnUpdate() 
+function GameOver:OnUpdate(tbgame) 
 	print("Updating GameOver State")
+	local stateMachine = tbgame.StateMachine
+	local contestants = tbgame.Contestants
+	local player1 = contestants.Player1
+	
+	stateMachine:ChangeState("EnemyTurn")
+	stateMachine:Update()
 end
 
-function GameOver:OnExit() 
+function GameOver:OnExit(tbgame) 
 	print("Exited GameOver State")
 end
 return GameOver
+
