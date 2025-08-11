eb811b1389ae5b387a190232af42b10f2314b060
LemonBunz <72637502+LemonBunz@users.noreply.github.com>
2025-08-11 14:21:21 +0800

    Update EnemyTurn.lua

--- GameState/EnemyTurn.lua
+++ GameState/EnemyTurn.lua
@@ -1,13 +1,20 @@
 local EnemyTurn = {}
-function EnemyTurn:OnEnter() 
+function EnemyTurn:OnEnter(tbgame) 
 	print("Entered EnemyTurn State")
 end
 
-function EnemyTurn:OnUpdate() 
+function EnemyTurn:OnUpdate(tbgame) 
 	print("Updating EnemyTurn State")
+	local stateMachine = tbgame.StateMachine
+	local contestants = tbgame.Contestants
+	local player1 = contestants.Player1
+	
+	stateMachine:ChangeState("EnemyTurn")
+	stateMachine:Update()
 end
 
-function EnemyTurn:OnExit() 
+function EnemyTurn:OnExit(tbgame) 
 	print("Exited EnemyTurn State")
 end
 return EnemyTurn
+
